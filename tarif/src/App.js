bb3dd81d71eeb8ed52013363df09bd13981f2ff2
Eleonora Maniecka <emaniecka34@gmail.com>
2022-12-25 22:01:35 +0100

    App.js update

--- tarif/src/App.js
+++ tarif/src/App.js
@@ -4,13 +4,11 @@ import Tarifs from './components/Tarifs';
 import "./styles/mainStyle.css";
 
 function App() {
+  const tCard = tariflist.map((tarif) => <Tarifs key={tarif.name} name={tarif.name} speed={tarif.speed} price={tarif.price} color={tarif.color}></Tarifs>)
 
   return (
     <div className='scssClass main'>
-      <Tarifs key={tariflist[0].name} name={tariflist[0].name} speed={tariflist[0].speed} price={tariflist[0].price} color={tariflist[0].color}></Tarifs>
-      <Tarifs key={tariflist[1].name} name={tariflist[1].name} speed={tariflist[1].speed} price={tariflist[1].price} color={tariflist[1].color}></Tarifs>
-      <Tarifs key={tariflist[2].name} name={tariflist[2].name} speed={tariflist[2].speed} price={tariflist[2].price} color={tariflist[2].color}></Tarifs>
-      <Tarifs key={tariflist[3].name} name={tariflist[3].name} speed={tariflist[3].speed} price={tariflist[3].price} color={tariflist[3].color}></Tarifs>
+      {tCard}
     </div>
   );
 }
